11d8679feeb944c0f5302e261ba088cab04be176
kpaul <kpaul@ethz.ch>
2024-12-12 15:16:13 +0100

    Multisolvent

---
+++ build/lib/GNNImplicitSolvent/__init__.py
@@ -0,0 +1,8 @@
+import yaml
+
+from Simulation.helper_functions import (
+    minimize_mol,
+    create_gnn_model,
+    calculate_entropy,
+)
+from MachineLearning.GNN_Models import GNN3_Multisolvent_embedding_run_multiple_Delta
